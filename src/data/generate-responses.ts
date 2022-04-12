8513fc35c9d52a98c02c8f888b3f8e5b80a65978
Nicolas Hery <hery.nicolas@gmail.com>
2022-04-12 10:32:49 -0400

    Refactor generate responses script

--- src/data/generate-responses.ts
+++ src/data/generate-responses.ts
@@ -1,26 +1,63 @@
+import * as fs from 'fs'
 import { faker } from '@faker-js/faker'
 import { stringify } from 'csv-stringify/sync'
 
-const count: number = 10
+type ResponseDefinition = {
+  question: string
+  response: ResponseGenerator
+}
 
-let currentResponseId: number = 1
-let responses: Array<Array<string>> = []
-responses.push([
-  'id',
-  'Are you a new or existing customer?',
-  'What is the item you would like to order?',
-  'What color(s) would you like to order?',
-])
-while (currentResponseId <= count) {
-  const response: Array<string> = [
-    currentResponseId + '',
-    faker.datatype.boolean()
-      ? faker.random.arrayElement([
-          'I am a new customer',
-          'I am an existing customer',
-        ])
-      : '',
-    faker.random.arrayElement([
+type ResponseGenerator = () => string
+
+const optional = (response: ResponseGenerator): ResponseGenerator => {
+  return () => (faker.datatype.boolean() ? response() : '')
+}
+
+const singleChoice = (choices: Array<string>): ResponseGenerator => {
+  return () => faker.random.arrayElement(choices)
+}
+
+const multipleChoices = (choices: Array<string>): ResponseGenerator => {
+  return () =>
+    faker.random
+      .arrayElements(
+        choices,
+        faker.datatype.number({ min: 1, max: choices.length })
+      )
+      .join(',')
+}
+
+const generateFormResponses = (
+  definitions: Array<ResponseDefinition>,
+  count: number
+): Array<Array<string>> => {
+  const result = []
+
+  // Header
+  result.push(['Response number'].concat(definitions.map((d) => d.question)))
+
+  // Responses
+  let currentResponseNumber: number = 1
+  while (currentResponseNumber <= count) {
+    result.push(
+      [currentResponseNumber + ''].concat(definitions.map((d) => d.response()))
+    )
+    currentResponseNumber++
+  }
+
+  return result
+}
+
+const orderRequestResponses: Array<ResponseDefinition> = [
+  {
+    question: 'Are you a new or existing customer?',
+    response: optional(
+      singleChoice(['I am a new customer', 'I am an existing customer'])
+    ),
+  },
+  {
+    question: 'What is the item you would like to order?',
+    response: singleChoice([
       'JF72HSY0',
       'AK10BBZ7',
       'MB917ZH5',
@@ -28,20 +65,18 @@ while (currentResponseId <= count) {
       '01N58XNS',
       '1928SJWN',
     ]),
-    faker.datatype.boolean()
-      ? faker.random
-          .arrayElements(
-            ['Red', 'Blue', 'Green', 'Yellow'],
-            faker.datatype.number({ min: 1, max: 4 })
-          )
-          .join(',')
-      : '',
-  ]
+  },
+  {
+    question: 'What color(s) would you like to order?',
+    response: optional(multipleChoices(['Red', 'Blue', 'Green', 'Yellow'])),
+  },
+]
 
-  responses.push(response)
-
-  currentResponseId++
+const run = () => {
+  fs.writeFileSync(
+    './src/data/order-request-responses.csv',
+    stringify(generateFormResponses(orderRequestResponses, 10))
+  )
 }
 
-console.log(stringify(responses))
-console.log()
+run()
